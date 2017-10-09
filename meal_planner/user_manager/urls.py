493bc102f6f7b306e39f9f5583c11e1dfa492c85
Kay McKelly <frecklestars@gmail.com>
2017-10-09 14:56:53 -0700

    Update url for login page path

--- meal_planner/user_manager/urls.py
+++ meal_planner/user_manager/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import RegisterUser
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='path/to/template/goes/here.html'), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='/registration/login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^password_change/$', auth_views.PasswordChangeView.as_view(template_name='path/to/template/goes/here.html'), name='password_change'),
     url(r'^password_change_done/$', auth_views.PasswordChangeDoneView.as_view(template_name='redirect/back/to/user/home.html'), name='password_change_done'),
