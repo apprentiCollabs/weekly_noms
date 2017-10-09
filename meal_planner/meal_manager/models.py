c3013d9f0795d4e9fc2a60c00379b210ed2719a1
Patrick <kaorti@gmail.com>
2017-10-09 12:33:39 -0700

    added recipe and ingredient models and taggit integration

--- meal_planner/meal_manager/models.py
+++ meal_planner/meal_manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='recipes'
+    )
+    tags = TaggableManager()
+    title = models.CharField(max_length=100)
+    instructions = models.TextField(max_length=1000)
+
+class Ingredient(models.Model):
+    recipes = models.ManyToManyField(
+        Recipe,
+        related_name='ingredients'
+    )
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    measurement = models.CharField(max_length=10)
