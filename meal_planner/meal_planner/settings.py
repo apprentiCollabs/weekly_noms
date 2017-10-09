0ce9fa91a8b426dfdc8fe953951b571a826afe81
Kay McKelly <frecklestars@gmail.com>
2017-10-09 10:25:01 -0700

    base.html built out in rough outline, css added in static dir, moving on to other basic template build-outs

--- meal_planner/meal_planner/settings.py
+++ meal_planner/meal_planner/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'meal_planner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/templates/', '/meal_manager/templates/', '/user_manager/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
