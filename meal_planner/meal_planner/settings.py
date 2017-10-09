0af23fda234ac3e8446abcb1933282b0b3ef2f8a
Patrick <kaorti@gmail.com>
2017-10-09 15:32:57 -0700

    installed postgres, updated requirements, changed settings to work of enviornment variables

--- meal_planner/meal_planner/settings.py
+++ meal_planner/meal_planner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*$^myy51ft)%$@ht*6w%=&wt4x!e%r4(qwcx8kfwng^9(qtuvq'
+SECRET_KEY = os.environ.get(SECRET_KEY, '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'meal_planner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2'',
+        'NAME': os.environ.get(DATABASE_NAME, ''),
+        'USER': os.environ.get(DATABASE_USER, ''),
+        'PASSWORD': os.environ.get(DATABASE_PASSWORD, ''),
+        'HOST': os.environ.get(DATABASE_HOST, ''),
+        'PORT': os.environ.get(DATABASE_PORT, ''),
+        'TEST': {
+            'NAME': 'test_database',
+        },
     }
 }
 
