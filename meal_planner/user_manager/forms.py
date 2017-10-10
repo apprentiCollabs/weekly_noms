cc0ca2f65cf1941c7e846f555be4919f29162605
Kay McKelly <frecklestars@gmail.com>
2017-10-10 13:07:10 -0700

    Pulled in delightful freshly merged stuff

--- meal_planner/user_manager/forms.py
+++ meal_planner/user_manager/forms.py
@@ -1,4 +1,5 @@
 from django.db import models
 from django.forms import ModelForm
 
-# TODO: login and signup, or is there a built-in you'd prefer for this?
+class RegForm(ModelForm):
+    class Meta: 
