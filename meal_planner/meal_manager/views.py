8a96a00f2db4b5b6c4e043aaa214198ea136c305
Patrick <kaorti@gmail.com>
2017-10-09 17:05:01 -0700

    first pass at the recipe display, edit and delete views. form integration pending

--- meal_planner/meal_manager/views.py
+++ meal_planner/meal_manager/views.py
@@ -1,26 +1,53 @@
+"""Process recipes into a meal plan."""
+
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from django.views.generic import(
+    DetailView, ListView, UpdateView, CreateView, DeleteView,
+)
+from .models import Recipe, Ingredient
+
+class AllRecipes(ListView):
+    """Home view for logged in users."""
+
+    queryset = Recipe.objects.filter(user=reqeuest.user)
+    template_name = 'meal_manager/all_recipes.html'
 
 
-class AllRecipes(View):
-    pass
-
-    # def get(self, request):
-    #     return render('all_recipes')
-
-
-class EditRecipe(View):
-    pass
+class AddRecipe(CreateView):
+    """Edit a single recipe."""
+    form_class =  # TODO: model form is imported and goes here
+    template_name = 'meal_manager/add.html'
+    success_url = reverse_lazy('meals:all_recipes')
 
 
-class ViewRecipe(View):
-    pass
+
+class EditRecipe(UpdateView):
+    """Edit a single recipe."""
+    
+    form_class =  # TODO: model form is imported and goes here
+    template_name = 'meal_manager/edit.html'
+    success_url = reverse_lazy('meals:all_recipes')
 
 
-class DeleteRecipe(View):
-    pass
+
+class ViewRecipe(DetailView):
+    """View a recipe in more complete detail."""
+    
+    model = Recipe
 
 
-class MealPlan(View):
+class DeleteRecipe(DeleteView):
+    """Remove an unwanted recipe."""
+    
+    model = Recipe
+    success_url = reverse_lazy('meals:all_recipes')
+
+
+class MealPlan(ListView):
+    """Generate a weighted-random meal plan."""
+    
     pass
