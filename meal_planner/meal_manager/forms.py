8089619c5ed27db6d4a45f68bc440fec11c7bd13
Kay McKelly <frecklestars@gmail.com>
2017-10-09 17:08:30 -0700

    Add modelform for recipe editing/adding

--- meal_planner/meal_manager/forms.py
+++ meal_planner/meal_manager/forms.py
@@ -1,2 +1,8 @@
 from django.db import models
 from django.forms import ModelForm
+
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['tags', 'title', 'ingredients', 'instructions']
--- meal_planner/user_manager/forms.py
+++ meal_planner/user_manager/forms.py
@@ -1,2 +1,4 @@
 from django.db import models
 from django.forms import ModelForm
+
+# TODO: login and signup, or is there a built-in you'd prefer for this?
