fcedf2afef13149cd3edf369649bc42f0e76e6d9
Kay McKelly <frecklestars@gmail.com>
2017-10-11 19:36:47 -0700

    INGREDIENT ADD WORKS WITH JS FINALLY OMG

--- meal_planner/meal_manager/static/js/addIngredient.js
+++ meal_planner/meal_manager/static/js/addIngredient.js
@@ -1,46 +1,92 @@
-var newIngredList = document.getElementsByTagName("LI");
-// var i;
-for (var i = 0; i < newIngredList.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  newIngredList[i].appendChild(span);
-}
+$(document).ready(function() {
+    // make add new ingredient column div appear
+    $("button#addIngredTrigger").click(function(){
+        $("aside#addIngredCol").toggle();
+    });
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-// var i;
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+    $("span#addNew").click(function(){
+        newElement();
+    });
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+    // what happens when you click add on the new ingredient input
+    function newElement() {
+        // get all close buttons
+        var allCloseBtns = document.getElementsByClassName("closeBtn");
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+        // creates new li and adds class
+        var newLi = document.createElement("li");
+        newLi.className = 'userAddedIngred';
 
-  for (var i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+        // get value from input box, makes it into a text node, and adds it to the newly created li
+        var inputValue = document.getElementById("newIngredInput").value;
+        var newIngredText = document.createTextNode(inputValue);
+        newLi.appendChild(newIngredText);
+
+        // handles error of blank input
+        if (inputValue === '') {
+            alert("You must write something!");
+        }
+        else {
+            document.getElementById("addedItems").appendChild(newLi);
+        }
+
+        // sets input to be blank
+        document.getElementById("newIngredInput").value = "";
+
+        // creates close buttons and appends them to newly added ingredients
+        var closeBtnSpan = document.createElement("span");
+        var closeButtonText = document.createTextNode("\u00D7");
+        closeBtnSpan.className = "closeBtn";
+        closeBtnSpan.appendChild(closeButtonText);
+        newLi.appendChild(closeBtnSpan);
+
+        // adds functionality for close buttons
+        for (var i = 0; i < allCloseBtns.length; i++) {
+            allCloseBtns[i].onclick = function() {
+                var newIngredLi = this.parentElement;
+                newIngredLi.remove();
+            }
+        }
     }
-  }
-}
+
+    // gives close buttons functionality to remove items from list
+    // var closeBtns = document.getElementsByClassName("closeBtn");
+
+    // create new ingredient from user input
+    // function addNewIngred() {
+        // creates new li and add class
+        // var newLi = document.createElement('li');
+        // li.className = 'newlyAdded';
+
+        // get value from user input and create text node, which is added to the new list item
+        // var inputValue = document.getElementById('newIngredInput').value;
+        // var newIngredText = document.createTextNode(inputValue);
+        // newLi.appendChild(newIngredText);
+
+        // deals with blank inputs added by accident
+        // if (inputValue === '') {
+        //     alert('New ingredient name cannot be blank.');
+        // }
+        // else {
+        //     document.getElementById('addedItems').appendChild(nnewLi);
+        // }
+
+        // resets new ingredient input to blank
+        // document.getElementById('newIngredInput').value = '';
+
+        // creates close buttons and appends to new ingredient list items
+        // var closeBtnSpan = document.createElement('span');
+        // var closeBtnText = document.createTextNode('\u00D7');
+        // closeBtnSpan.className = 'close';
+        // closeBtnSpan.appendChild(closeBtnText);
+        // newLi.appendChild(closeBtnSpan);
+
+        // makes close buttons remove items
+//         for (var i = 0; i < closeBtns.length; i++) {
+//             closeBtns[i].onclick = function() {
+//                 var actualLi = document.getElementsByClassName('ingredListItem');
+//                 actualLi.remove();
+//             }
+//         }
+//     }
+})
