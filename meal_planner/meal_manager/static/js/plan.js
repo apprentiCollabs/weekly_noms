6ee38d54ded97cbc7a349eca7f4e9fb8d0f94a85
Kay McKelly <frecklestars@gmail.com>
2017-10-12 12:47:49 -0700

    Drag/drop finally working, w00t!

--- meal_planner/meal_manager/static/js/plan.js
+++ meal_planner/meal_manager/static/js/plan.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
-    function addDrag() {
-        var talks = Array.prototype.slice.call(document.querySelectorAll(".category"));
-        var recipes = Array.prototype.slice.call(document.querySelectorAll(".indRecipe"));
-        var containers = talks.concat(recipes);
-
-        dragula(containers).on('drop', function(el, target, source, sibling) {
-            if (target.classList.contains('recipes')) {
-                // hide drag & drop hint
-                target.firstChild.data = '';
-                target.style.border = '';
-            }
-        })
-
-        if (source.classList.contains('session-talks')) {
-            // restore drag & drop hint
-            if (source.childNodes.length == 1) {
-            source.firstChild.data = 'Drag talks here';
-            }
-        }
+    function $(id) {
+        return document.getElementById(id);
     }
+
+    dragula([$('availableRecipes'), $('sunday'), $('monday'), $('tuesday'), $('wednesday'), $('thursday'), $('friday'), $('saturday'),], { revertOnSpill: true
+    }).on('drop', function(el) {
+
+    });
 })
