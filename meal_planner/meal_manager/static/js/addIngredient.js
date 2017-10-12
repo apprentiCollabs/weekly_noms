b990b6aab5cfd8666bc9f39e0ed2887bb7428272
Kay McKelly <frecklestars@gmail.com>
2017-10-11 19:37:47 -0700

    Code cleanup

--- meal_planner/meal_manager/static/js/addIngredient.js
+++ meal_planner/meal_manager/static/js/addIngredient.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
         $("aside#addIngredCol").toggle();
     });
 
+    // add click listener on add new button, invokes function below
     $("span#addNew").click(function(){
         newElement();
     });
 
-    // what happens when you click add on the new ingredient input
     function newElement() {
         // get all close buttons
         var allCloseBtns = document.getElementsByClassName("closeBtn");
@@ -48,45 +48,4 @@ $(document).ready(function() {
             }
         }
     }
-
-    // gives close buttons functionality to remove items from list
-    // var closeBtns = document.getElementsByClassName("closeBtn");
-
-    // create new ingredient from user input
-    // function addNewIngred() {
-        // creates new li and add class
-        // var newLi = document.createElement('li');
-        // li.className = 'newlyAdded';
-
-        // get value from user input and create text node, which is added to the new list item
-        // var inputValue = document.getElementById('newIngredInput').value;
-        // var newIngredText = document.createTextNode(inputValue);
-        // newLi.appendChild(newIngredText);
-
-        // deals with blank inputs added by accident
-        // if (inputValue === '') {
-        //     alert('New ingredient name cannot be blank.');
-        // }
-        // else {
-        //     document.getElementById('addedItems').appendChild(nnewLi);
-        // }
-
-        // resets new ingredient input to blank
-        // document.getElementById('newIngredInput').value = '';
-
-        // creates close buttons and appends to new ingredient list items
-        // var closeBtnSpan = document.createElement('span');
-        // var closeBtnText = document.createTextNode('\u00D7');
-        // closeBtnSpan.className = 'close';
-        // closeBtnSpan.appendChild(closeBtnText);
-        // newLi.appendChild(closeBtnSpan);
-
-        // makes close buttons remove items
-//         for (var i = 0; i < closeBtns.length; i++) {
-//             closeBtns[i].onclick = function() {
-//                 var actualLi = document.getElementsByClassName('ingredListItem');
-//                 actualLi.remove();
-//             }
-//         }
-//     }
 })
